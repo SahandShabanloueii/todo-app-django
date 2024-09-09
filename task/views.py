c2e107c513cd15fb1f651f0134eecb4ec81fdee6
Sahand Shabanlouei <sahandshabanlouei@gmail.com>
2024-09-09 17:00:04 +0330

    Add show detail and delete_task functionality to app

--- task/views.py
+++ task/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect
 from task.models import TodoTask, Category
-from task.forms import TaskCreatForms
+from task.forms import TaskCreateForm
+from django.views.generic import DetailView
+from django.contrib import messages
+
 
 def show_task(request):
     query = TodoTask.objects.all().order_by('created')
-
-    form = TaskCreatForms(request.POST)
+    
+    # createform part
+    form = TaskCreateForm(request.POST)
     if form.is_valid():
         form.save()
+        messages.success(request, "Task is added successfuly")
         return HttpResponseRedirect('/')
     else:
-        form = TaskCreatForms()
-
+        form = TaskCreateForm()
+        
     dic = {
         'query' : query,
-        'form' : form,
+        'form' : form, 
     }
-
+    
     return render(request, 'task/index.html', dic)
+
+
+
+class TaskDetail(DetailView):
+    model = TodoTask
+    template_name = 'task/detail.html'
+    
+    
+def delete_task(request, id):
+    task = get_object_or_404(TodoTask, id=id)
+    task.delete()
+    return redirect('show_task')
